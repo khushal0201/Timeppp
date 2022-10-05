dd54f4e61c6cc5586430b221994c77c916ad6cc4
khushal0201 <khushalmakhija201@gmail.com>
2022-10-06 04:43:51 +0530

    get thingy

--- src/main/java/com/inc/typ.java
+++ src/main/java/com/inc/typ.java
@@ -3,6 +3,7 @@ package com.inc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,6 +13,15 @@ public class typ{
 	      @RequestMapping(value="/helo",method=RequestMethod.GET)
 	      @ResponseBody
 	      public String sm() {
-	    	  return "<h1>hello bc</h1>";
+	    	  return "<h1>hello bc</h1>"
+	    	  		+ "<form action='sing'>"
+	    	  		+ "enter name:<input type='button' name='yn' placeholder='yo mama'/>"
+	    	  		+ "<input type='submit'/>"
+	    	  		+ "</form>";
+	      }
+	      @RequestMapping(value="/sing",method=RequestMethod.GET)
+	      @ResponseBody
+	      public String ccm(@RequestParam("yn") String name) {
+	    	             return "<h1>Bruh yo name is "+name+"</h1>";
 	      }
 }
